79c901ec8302ad1552d9175475104834f779f1c7
jmuri <jmuri@umass.edu>
2019-01-30 17:55:08 -0500

    Update TexturePainter.cs

--- ARWhiteboard/Assets/3D painter/TexturePainter.cs
+++ ARWhiteboard/Assets/3D painter/TexturePainter.cs
@@ -7,29 +7,34 @@ public class TexturePainter : MonoBehaviour {
     
 
     [Range(1, 2048)]
-    public int canvasWidth;
+    private int canvasWidth;
     [Range(1, 2048)]
-    public int canvasHeight;
+    private int canvasHeight;
+    private int radius;
 
-    public Color foregroundColor = Color.black;
-    public Color backgroundColor = Color.white;
+    private bool marker;
 
-    
+    private Color foregroundColor;
+    private Color backgroundColor;
+    private Color prevColor;
+
     Texture2D canvas;
     
-    //1024x768
+    //1024x768 is the IR input space
     
     void Start()
     {
+        foregroundColor = Color.black;
+        backgroundColor = Color.white;
+        radius = 6;
+        marker = true;
         CreateBackground();
-        //Fill(backgroundColor, canvas);
     }
 
     void Update()
     {
         if (Input.GetKey(KeyCode.Mouse0))
-            MousePressed();
-            
+            MousePressed();            
     }
 
     void CreateBackground()
@@ -38,83 +43,78 @@ public class TexturePainter : MonoBehaviour {
         canvasWidth = tex.width;
         canvasHeight = tex.height;
         canvas = new Texture2D(canvasWidth, canvasHeight, TextureFormat.ARGB32, false);
-        //canvas.filterMode = FilterMode.Point;
-        //var filePath = "C:/Users/Jeff/Pictures/back.png";
-        //if (System.IO.File.Exists(filePath))
-        //{
-        // Image file exists - load bytes into texture
-        //var bytes = System.IO.File.ReadAllBytes(filePath);
-        
-        
-        //tex.LoadImage(bytes);
-            
-        //GetComponent<MeshRenderer>().material.SetTexture("_MainTex", tex);
-        //Fill(backgroundColor, tex.GetPixels(), canvas);
-        //canvas.EncodeToPNG();
         canvas.SetPixels(tex.GetPixels());
-        //canvas.LoadImage(bytes);
-
-
-        //}
-        
-        //canvas.filterMode = FilterMode.Point;
         GetComponent<MeshRenderer>().material.SetTexture("_MainTex", canvas);
         canvas.Apply();
-        
-
-        // Load the Image from somewhere on disk
-
-    }
-
-    void Fill(Color color, Color[] image, Texture2D texture)
-    {
-        
-        Color[] pixels = texture.GetPixels();
-        for (int i = 0; i < pixels.Length; i++)
-        {
-            pixels[i] = color;
-        }
-        texture.SetPixels(pixels);
-        texture.Apply();
+        prevColor = foregroundColor;
     }
 
     void MousePressed()
-    {
-        
+    {        
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hitInfo;
         if(Physics.Raycast(ray, out hitInfo))
         {
             var pixelCoords = uv2PixelCoords(hitInfo.textureCoord);
             //800
+            //draw
             if (pixelCoords.x < 800)
             {
                 Stroke(pixelCoords.x, pixelCoords.y, foregroundColor);
             }
-            else if (pixelCoords.x > 837 && pixelCoords.x < 986 && pixelCoords.y  < 458 && pixelCoords.y > 341)
+            //change color
+            else if (pixelCoords.x > 837 && pixelCoords.x < 986 && 
+                pixelCoords.y  < 946 && pixelCoords.y > 737 && canvas.GetPixel(pixelCoords.x, pixelCoords.y) != backgroundColor)
             {
                 foregroundColor = canvas.GetPixel(pixelCoords.x, pixelCoords.y);
+                prevColor = canvas.GetPixel(pixelCoords.x, pixelCoords.y);
             }
-        }
-    }
-
-    void Stroke(int x, int y, Color color)
-    {
-        
-        //x left 837 x right 986 y top 458 y bottom 341
-        //Debug.Log("x: " + x + ", y: " + y);
-        for (int i = x-9; i <= x+9; i++)
-        {
-            for (int j = y-9; j <= y+9; j++)
+            //change size1
+            else if (pixelCoords.x > 814 && pixelCoords.x < 852 && 
+                pixelCoords.y < 641 && pixelCoords.y > 449 && radius > 2)
             {
-                if (j < canvasHeight && j > 0 && i < canvasWidth - 278 && i > 0)
-                    canvas.SetPixel(i, j, color);
+                radius--;
+                /*
+                GetComponent<MeshCollider>().enabled = false;
+                StartCoroutine(Wait(1));
+                GetComponent<MeshCollider>().enabled = true;*/
+            }
+            //change size2
+            else if (pixelCoords.x > 852 && pixelCoords.x < 935 && pixelCoords.y < 641 && pixelCoords.y > 449)
+            {
+                radius = 6;
+            }
+            //change size3
+            else if (pixelCoords.x > 935 && pixelCoords.x < 1009 &&
+                pixelCoords.y < 641 && pixelCoords.y > 449 && radius < 50)
+            {
+                radius += 1;
+            }
+            //marker tool
+            else if (pixelCoords.x > 814 && pixelCoords.x < 910 && pixelCoords.y < 382 && pixelCoords.y > 188)
+            {
+                marker = true;
+                foregroundColor = prevColor;
+            }
+            //eraser tool
+            else if (pixelCoords.x > 915 && pixelCoords.x < 1009 && pixelCoords.y < 382 && pixelCoords.y > 188)
+            {
+                marker = false;
+                foregroundColor = backgroundColor;   
+            }
+            //clear all
+            else if (pixelCoords.x > 814 && pixelCoords.x < 910 && pixelCoords.y < 166 && pixelCoords.y > 13)
+            {
+                for (int i = 0; i <= 800; i++)
+                {
+                    for (int j = 0; j <= canvasHeight; j++) 
+                        canvas.SetPixel(i, j, backgroundColor);
+                }
+                canvas.Apply();
             }
         }
-        
-        canvas.Apply();
     }
-
+    
     Vector2Int uv2PixelCoords(Vector2 uv)
     {
         int x = Mathf.FloorToInt(uv.x * canvasWidth);
@@ -122,6 +122,27 @@ public class TexturePainter : MonoBehaviour {
         return new Vector2Int(x, y);
     }
 
-
-
+    void Stroke(int x, int y, Color color)
+    {
+        //Debug.Log("x: " + x + ", y: " + y);
+        for (int yn = -radius; yn <= radius; yn++)
+        {
+            for (int xn = -radius; xn <= radius; xn++)
+            {
+                if(y + yn < canvasHeight && y + yn > 0 && x + xn < 800 && x + xn > 0)
+                {
+                    if (xn * xn + yn * yn <= radius * radius && marker)
+                        canvas.SetPixel(x + xn, y + yn, color);
+                    else if(!marker) 
+                        canvas.SetPixel(x + xn, y + yn, color);
+                }
+            }
+        }
+        canvas.Apply();
+    }
+    /*
+    IEnumerator Wait(float sec)
+    {
+        yield return new WaitForSecondsRealtime(sec);
+    }*/
 }
