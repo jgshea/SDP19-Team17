6dad36c0124ed757a2c8afb1804fd82f0941f05c
jmuri <jmuri@umass.edu>
2019-01-30 22:03:34 -0500

    camera drawing, line fill finished

--- ARWhiteboard/Assets/3D painter/TexturePainter.cs
+++ ARWhiteboard/Assets/3D painter/TexturePainter.cs
@@ -1,17 +1,24 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using System.IO.Ports;
+using System.Text;
+
 
 public class TexturePainter : MonoBehaviour {
 
-    
+    SerialPort stream = new SerialPort("COM4", 38400); //Set the port (com4) and the baud rate (9600, is standard on most devices)
+	string value = "";
+	string[] values;
 
     [Range(1, 2048)]
     private int canvasWidth;
     [Range(1, 2048)]
     private int canvasHeight;
     private int radius;
-
+	private int prevX;
+	private int prevY;
     private bool marker;
 
     private Color foregroundColor;
@@ -29,12 +36,18 @@ public class TexturePainter : MonoBehaviour {
         radius = 6;
         marker = true;
         CreateBackground();
+		stream.Open();
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
-            MousePressed();            
+		stream.ReadTimeout = 103;
+		value = stream.ReadLine(); //Read the information
+		values = value.Split(',');
+		stream.BaseStream.Flush();
+		MousePressed();
+        //if (Input.GetKey(KeyCode.Mouse0))
+        //    MousePressed();            
     }
 
     void CreateBackground()
@@ -51,78 +64,93 @@ public class TexturePainter : MonoBehaviour {
 
     void MousePressed()
     {        
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hitInfo;
-        if(Physics.Raycast(ray, out hitInfo))
-        {
-            var pixelCoords = uv2PixelCoords(hitInfo.textureCoord);
-            //800
-            //draw
-            if (pixelCoords.x < 800)
-            {
-                Stroke(pixelCoords.x, pixelCoords.y, foregroundColor);
-            }
-            //change color
-            else if (pixelCoords.x > 837 && pixelCoords.x < 986 && 
-                pixelCoords.y  < 946 && pixelCoords.y > 737 && canvas.GetPixel(pixelCoords.x, pixelCoords.y) != backgroundColor)
-            {
-                foregroundColor = canvas.GetPixel(pixelCoords.x, pixelCoords.y);
-                prevColor = canvas.GetPixel(pixelCoords.x, pixelCoords.y);
-            }
-            //change size1
-            else if (pixelCoords.x > 814 && pixelCoords.x < 852 && 
-                pixelCoords.y < 641 && pixelCoords.y > 449 && radius > 2)
-            {
-                radius--;
-                /*
-                GetComponent<MeshCollider>().enabled = false;
-                StartCoroutine(Wait(1));
-                GetComponent<MeshCollider>().enabled = true;*/
-            }
-            //change size2
-            else if (pixelCoords.x > 852 && pixelCoords.x < 935 && pixelCoords.y < 641 && pixelCoords.y > 449)
-            {
-                radius = 6;
-            }
-            //change size3
-            else if (pixelCoords.x > 935 && pixelCoords.x < 1009 &&
-                pixelCoords.y < 641 && pixelCoords.y > 449 && radius < 50)
-            {
-                radius += 1;
-            }
-            //marker tool
-            else if (pixelCoords.x > 814 && pixelCoords.x < 910 && pixelCoords.y < 382 && pixelCoords.y > 188)
-            {
-                marker = true;
-                foregroundColor = prevColor;
-            }
-            //eraser tool
-            else if (pixelCoords.x > 915 && pixelCoords.x < 1009 && pixelCoords.y < 382 && pixelCoords.y > 188)
-            {
-                marker = false;
-                foregroundColor = backgroundColor;   
-            }
-            //clear all
-            else if (pixelCoords.x > 814 && pixelCoords.x < 910 && pixelCoords.y < 166 && pixelCoords.y > 13)
-            {
-                for (int i = 0; i <= 800; i++)
-                {
-                    for (int j = 0; j <= canvasHeight; j++) 
-                        canvas.SetPixel(i, j, backgroundColor);
-                }
-                canvas.Apply();
-            }
-        }
+       // Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+       // RaycastHit hitInfo;
+		int x = Convert.ToInt32(values[0]);
+		int y = Convert.ToInt32(values[1]);
+		
+		if(x == 1023 && y == 1023)
+		{
+			prevX = -1;
+			prevY = -1;
+		}
+	   
+       // if(Physics.Raycast(ray, out hitInfo))
+        //{
+         //   var pixelCoords = uv2PixelCoords(hitInfo.textureCoord);
+		//800
+		//draw
+		if (x < 800)
+		{
+			if(prevX == -1 || prevY == -1)
+			{
+				prevX = x;
+				prevY = y;
+				//Stroke(x, y, foregroundColor);
+			}
+			DrawLine(prevX, prevY, x, y, foregroundColor);
+			prevX = x;
+			prevY = y;
+		}
+		//change color
+		else if (x > 837 && x < 986 && y  < 461 && y > 340 && canvas.GetPixel(x, y) != backgroundColor)
+		{
+			foregroundColor = canvas.GetPixel(x, y);
+			prevColor = canvas.GetPixel(x, y);
+		}
+		//change size1
+	    else if (x > 814 && x < 879 && y < 285 && y >= 169 && radius > 2)
+		{
+			radius--;
+			/*
+			GetComponent<MeshCollider>().enabled = false;
+			StartCoroutine(Wait(1));
+			GetComponent<MeshCollider>().enabled = true;*/
+		}
+		//change size2
+		else if (x > 879 && x <= 942 && y < 285 && y >= 169)
+		{
+			radius = 6;
+		}
+		//change size3
+		else if (x > 942 && x <= 1010 && y < 285 && y >= 169 && radius < 50)
+		{
+			radius += 1;
+		}
+		//marker tool
+		else if (x > 911 && x <= 1010 && y < 139 && y >= 72)
+		{
+			marker = true;
+			foregroundColor = prevColor;
+		}
+		//eraser tool
+		else if (x > 814 && x <= 911 && y < 139 && y >= 72)
+		{
+			marker = false;
+			foregroundColor = backgroundColor;   
+		}
+		//clear all
+		else if (x > 911 && x < 1010 && y < 72 && y >= 12)
+		{
+			for (int i = 0; i <= 800; i++)
+			{
+				for (int j = 0; j <= canvasHeight; j++) 
+					canvas.SetPixel(i, j, backgroundColor);
+			}
+			canvas.Apply();
+		} 
+	//}
     }
-    
+  /*  
     Vector2Int uv2PixelCoords(Vector2 uv)
     {
         int x = Mathf.FloorToInt(uv.x * canvasWidth);
         int y = Mathf.FloorToInt(uv.y * canvasHeight);
         return new Vector2Int(x, y);
     }
-
-    void Stroke(int x, int y, Color color)
+  */
+  
+    void DrawCircle(int x, int y, Color color)
     {
         //Debug.Log("x: " + x + ", y: " + y);
         for (int yn = -radius; yn <= radius; yn++)
@@ -138,8 +166,55 @@ public class TexturePainter : MonoBehaviour {
                 }
             }
         }
-        canvas.Apply();
-    }
+        
+    } 
+	
+	void Bresenham(int x0, int y0, int x1, int y1, bool reversedAxis, Color color)
+	{
+		double deltax = x1 - x0;
+		double deltay = y1 - y0;
+		double deltaerr = Math.Abs(deltay / deltax);    
+		double error = 0.0f; 
+		int y = y0;
+		for (int x = x0; x <= x1; x++)
+		{
+			if(reversedAxis)
+				DrawCircle(y, x, color);
+			else
+				DrawCircle(x, y, color);
+			error = error + deltaerr;
+			if (error >= 0.5f)
+			{
+				y = y + Math.Sign(deltay) * 1;
+				error = error - 1.0f;
+			}
+		}
+		canvas.Apply();
+	}
+	
+	void DrawLine(int x0, int y0, int x1, int y1, Color color)
+	{
+		if(Math.Abs(y1 - y0) < Math.Abs(x1 - x0))
+			if(x0 <= x1)
+				Bresenham(x0, y0, x1, y1, false, color);
+			else
+				Bresenham(x1, y1, x0, y0, false, color);
+		else
+			if(y0 <= y1)
+				Bresenham(y0, x0, y1, x1, true, color);
+			else
+				Bresenham(y1, x1, y0, x0, true, color);
+	}
+	
+	void OnGUI(){
+	   //string newString = "Connected: " + transform.rotation.x + ", " + transform.rotation.y + ", " + transform.rotation.z;
+	   GUI.Label(new Rect(10,10,300,100), value); //Display new values
+	   GUI.Label(new Rect(10,30,300,100), values[0]); //Display new values
+	   GUI.Label(new Rect(10,50,300,100), values[1]); //Display new values
+
+	   // Though, it seems that it outputs the value in percentage O-o I don't know why.
+	}
+	
     /*
     IEnumerator Wait(float sec)
     {
